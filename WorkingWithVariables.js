ba13e708d11a70896c3143149bae0d68ba00e076
Sue <suemurray.2012@gmail.com>
2022-04-22 11:25:26 +0100

    Add first javascript. Declaring and Assigning variables

---
+++ WorkingWithVariables.js
@@ -0,0 +1,14 @@
+
+//Declaring and assigning
+
+let admin; // Define variable admin
+let Name; //Define variable name
+Name =  "John"; //Assign John to variable name
+admin = Name; //Assign admin the same as John 
+console.log( admin );
+
+//Giving descriptive variable names 
+
+
+let ourPlanetName = "MARS...oh no I meant Earth" //Declare and assign our planets name
+let currentVistorName = "John" //Current vistor to a website 
